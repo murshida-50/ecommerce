7eeb730a6f7f5156d6f20b2c5661667890cd949c
murshida-50 <murshidaa50@gmail.com>
2022-02-06 11:01:43 +0600

    first commit

---
+++ lib/const/custombutton.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+Widget CustomButton(String bottomText,onPressed){
+  return SizedBox(
+    width: 1.sw,
+    height: 56.w,
+    child: ElevatedButton(onPressed: onPressed,
+     child: Text(
+       bottomText,
+       style: TextStyle(color: Colors.white,fontSize: 18.sp),
+     ),
+     ),
+  );
+}
